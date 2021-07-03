f1378bb596ef9df066247e667b0193d94154d1ac
changun <changun@192.168.130.128>
2021-07-03 11:49:59 +0800

    对目录结果做了调整

--- ProDir/shqx/zhaoxianyou/killall.sh
+++
@@ -1,5 +0,0 @@
-killall ftpgetfile crtsurfdata psurfdata deletetables  crtsurfdata1 psurfdata1 wgetrain24 wgettemp24 pzhrain24file
-
-sleep 5
-
-killall -9 ftpgetfile crtsurfdata psurfdata deletetables  crtsurfdata1 psurfdata1 wgetrain24 wgettemp24 pzhrain24file
